b56d95a14a759de01edcbc9fac5ed120b3f6637b
Ngumah <fagwaguy@gmail.com>
2022-12-15 18:13:46 +0000

    Checking letter cases

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isupper - check upper cases
+ *  @c: value to check
+ * Return: 1 if successful else 0 
+ */
+int _isupper(int c)
+{
+	char k;
+	for (k = 'A'; k <= 'Z' k++)
+	{
+		if (c != k)
+		{
+			return(0);
+		}
+		else
+		{
+			return(1);
+			break:
+		}
+	}
+}	
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+nt _isupper(int c);
+
+#endif
