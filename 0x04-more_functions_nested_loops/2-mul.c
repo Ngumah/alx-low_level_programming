7890ef69142d2aa8b0acc2d8bae3415be3630f5a
Ngumah <fagwaguy@gmail.com>
2022-12-16 23:58:31 +0000

    Printing numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - multiplies two whole numbers
+ * @a: first number
+ * @b: second number
+ * Return: multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
