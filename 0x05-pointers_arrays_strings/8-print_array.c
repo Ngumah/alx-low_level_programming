efc4a8c7984843f082a19690b3b40fcf4b0e8e8f
Ngumah <fagwaguy@gmail.com>
2022-12-19 15:04:00 +0000

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - print the elements of an array
+ * @a: array
+ * @n: length of array
+ *
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		if (i != n - 1)
+		{
+			printf("%d, ", *(a + i) + 48);
+		}
+		else
+		{
+			printf("%d\n", *(a + i));
+		}
+	}
+}
