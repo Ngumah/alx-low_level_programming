af99ca36befea24955ca5ca4fe3bb501534d93f1
Ngumah <fagwaguy@gmail.com>
2022-12-19 14:22:18 +0000

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+/**
+ * rev_string - reverse a given string
+ * @s: string
+ *
+ */
+void rev_string(char *s)
+{
+	int i = 0, j = 0, len, tmp;
+
+	while (s[j])
+		j++;
+	len = j;
+	j--;
+
+	for (; i <= len / 2; i++)
+	{
+		if (i != j)
+		{
+			tmp = s[i];
+			s[i] = s[j];
+			s[j] = tmp;
+			j--;
+		}
+	}
+}
