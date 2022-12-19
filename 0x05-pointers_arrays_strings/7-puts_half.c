bb9a2932afd5020337f63752f62a0059b6299cff
Ngumah <fagwaguy@gmail.com>
2022-12-19 14:44:10 +0000

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to be printed
+ *
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (str[i++]) 
+		;
+
+	if ((i / 2) % 2 == 1)
+		i = i / 2 + 1;
+	else
+		i /= 2;
+
+	for (; str[i]; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
