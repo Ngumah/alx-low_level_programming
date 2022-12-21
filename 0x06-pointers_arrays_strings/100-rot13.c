5d6595a5dc0db18f1271d05650c8d6a4e9698fb1
Ngumah <fagwaguy@gmail.com>
2022-12-21 17:40:57 +0000

    task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rot13 - encode the string using rot13
+ * @str: string
+ *
+ * Return: leet string
+ */
+char *rot13(char *str)
+{
+	int i;
+	int c = 0;
+	char lower[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		'y', 'z'};
+	char rot_arr[52] = { 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+		'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+		'h', 'i', 'j', 'k', 'l', 'm'};
+
+	while (str[c])
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (str[c] == lower[i])
+				str[c] = rot_arr[i];
+		}
+		c++;
+	}
+	return (str);
+}
