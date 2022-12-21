6434131a69a4a58284bebe7f778ba202a8429b18
Ngumah <fagwaguy@gmail.com>
2022-12-21 18:17:53 +0000

    task 101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer to std output
+ * @n: integer
+ *
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = - n;
+	}
+	if (n / 10 == 0)
+	{
+		_putchar('0' + n % 10);
+		return;
+	}
+	print_number(n / 10);
+	_putchar('0' + n % 10);
+}
