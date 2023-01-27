0c10ccf633f0a029b200dc350ac3dc22c5a584e7
Ngumah <fagwaguy@mail.com>
2023-01-27 21:24:42 +0000

    hot fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -33,5 +34,7 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 
 void free_list(list_t *head);
+
+
 
 #endif
